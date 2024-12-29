c0b17275f1a5d50a536572d94fe3d4a734a9a668
IsaiahMitchell <zaymgmt707@gmail.com>
2024-12-29 04:31:53 -0800

    updates components

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react"
 
 function Item({ name, category }) {
+
+  const [inCart, setInCart] = useState(false)
+
+  const handleCartToggle = () => {
+    setInCart(!inCart)
+  }
+
+  const liClassName = inCart ? "in-cart" : ""
+
+  const buttonText = inCart ? "Remove From Cart" : "Add to Cart"
+
   return (
-    <li className="">
+    <li className={liClassName}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>
+        {buttonText}
+      </button>
     </li>
-  );
-}
+  )
+};
 
 export default Item;
